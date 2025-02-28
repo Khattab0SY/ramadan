44ac92a7fb57178032239df96934334d10d5fc9c
Khattab0SY <momoat72@gmail.com>
2025-02-28 21:30:16 +0200

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Lazypage from './lazypage.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/lazypage" element={<Lazypage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
